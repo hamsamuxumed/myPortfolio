453de3ed6600a2b5aed2b156b95e1f16ce514042
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-13 16:57:50 +0000

    add new section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { Link, Container, Box, Heading, Image, Button, useColorModeValue } from 
 import Paragraph from "../components/paragraph"
 import Section from "../components/section"
 import NextLink from 'next/link'
+import { ChevronRightIcon } from "@chakra-ui/icons"
+import { BioSection, BioYear } from "../components/bio"
 
 
 
@@ -15,6 +17,7 @@ function Home() {
         p={3}
         mb={6}
         textAlign="center"
+
       >
         Hello, I&apos;m a full-stack developer based in the UK
       </Box>
@@ -52,7 +55,7 @@ function Home() {
         </Heading>
         <Paragraph>
 
-          Art, video games and football enthusiast, with a passion for web development. I am a person who loves to learn and gain new skills, the empowering feeling of crafting a project from scratch and the satisfaction of problem solving is why I pursued a career in tech.
+          Art, video games and football enthusiast, with a passion for web development. I am a person who loves to learn and gain new skills, the empowering feeling of crafting a project from scratch and the satisfaction of problem solving is why I pursued a career in tech. {' '}
           <NextLink href='./works'>
             <Link>Work</Link>
           </NextLink>
@@ -60,12 +63,24 @@ function Home() {
         </Paragraph>
         <Box align='center' my={4}>
           <NextLink href='./works'>
-            <Button >
+            <Button rightIcon={<ChevronRightIcon />} colorScheme='blue' >
               My Portfolio
             </Button>
           </NextLink>
         </Box>
       </Section >
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear> 1997
+          </BioYear>
+          hello - hello
+        </BioSection>
+      </Section>
+
 
     </Container>
 
